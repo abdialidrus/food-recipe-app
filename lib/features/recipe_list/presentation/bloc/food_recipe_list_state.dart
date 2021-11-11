6e61cc8a5378a475b93c004d1ef93f2b0e8ff3ec
abdialidrus <muhammadabdi25@gmail.com>
2021-11-11 14:54:49 +0700

    update recipe list feature state

--- lib/features/recipe_list/presentation/bloc/food_recipe_list_state.dart
+++ lib/features/recipe_list/presentation/bloc/food_recipe_list_state.dart
@@ -2,9 +2,29 @@ part of 'food_recipe_list_bloc.dart';
 
 abstract class FoodRecipeListState extends Equatable {
   const FoodRecipeListState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-class FoodRecipeListInitial extends FoodRecipeListState {}
+class Initial extends FoodRecipeListState {}
+
+class Loading extends FoodRecipeListState {}
+
+class Loaded extends FoodRecipeListState {
+  final FoodRecipeList recipeList;
+
+  const Loaded({required this.recipeList});
+
+  @override
+  List<Object> get props => [recipeList];
+}
+
+class Error extends FoodRecipeListState {
+  final String message;
+
+  const Error({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
